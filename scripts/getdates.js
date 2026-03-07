386ce5766c74b5e0e741b0106220dac27fc1d933
luislopezpar14-commits <luis.lopezpar14@gmail.com>
2026-03-07 17:43:51 -0600

    week 1 assigment homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+
+// Get current year and display in footer
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Get last modified date
+document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
